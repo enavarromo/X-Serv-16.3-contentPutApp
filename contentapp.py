24cf6a012529cdd5414009e4baf9bbaa41972c7d
Eloy Navarro Morales <enavarro@zeta40.aulas.gsyc.es>
2016-03-08 10:58:09 +0100

    Apuntes pre POST

--- contentapp.py
+++ contentapp.py
@@ -1,50 +1,45 @@
 #!/usr/bin/python
-
-"""
- contentApp class
- Simple web application for managing content
-
- Copyright Jesus M. Gonzalez-Barahona, Gregorio Robles 2009-2015
- jgb, grex @ gsyc.es
- TSAI, SAT and SARO subjects (Universidad Rey Juan Carlos)
- October 2009 - March 2015
-"""
-
 import webapp
 
 
 class contentApp (webapp.webApp):
-    """Simple web application for managing content.
 
-    Content is stored in a dictionary, which is intialized
-    with the web content."""
-
-    # Declare and initialize content
+    # Variable de clase
     content = {'/': 'Root page',
-               '/page': 'A page'
+               '/page': 'A page',
+               '/pepe': 'Hola',
+               '/nueva':'Otra mas'
                }
 
     def parse(self, request):
-        """Return the resource name (including /)"""
-
-        return request.split(' ', 2)[1]
+        
+        herader = request.split(' ', 1)[0]  # metodo
+        request = request.split(' ', 2)[1]  # recurso
+        try
+            body = request.split()[-1]  # body = request.split('\r\n\r\n')[1]
+        except IndexError:
+            body = ""
+        return [header, request, body]
 
     def process(self, resourceName):
-        """Process the relevant elements of the request.
 
-        Finds the HTML text corresponding to the resource name,
-        ignoring requests for resources not in the dictionary.
-        """
-
-        if resourceName in self.content.keys():
+        metodo, recurso, cuerpo = peticion
+        if metodo == "GET":
+            if recurso in self.content: # self.content.keys()
+                httpCode = "200 OK"
+                htmlBody = "<html><body>" + self.content[resourceName] \
+                    + "</body></html>"
+            else:
+                httpCode = "404 Not Found"
+                htmlBody = "Not Found"
+        elif metodo == "PUT" or metodo == "POST"
+            self.content[recurso] = cuerpo
             httpCode = "200 OK"
-            htmlBody = "<html><body>" + self.content[resourceName] \
-                + "</body></html>"
-        else:
-            httpCode = "404 Not Found"
-            htmlBody = "Not Found"
+            htmlBody = "Todo ha ido bien"
+        else
+            httpCode = "450 Method Not Allowed"
+            htmlBody = "Go away!"
         return (httpCode, htmlBody)
 
-
 if __name__ == "__main__":
-    testWebApp = contentApp("localhost", 1234)
+    testWebApp = contentApp("localhost", 1235)
